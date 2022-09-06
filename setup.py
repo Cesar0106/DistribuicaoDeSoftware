8a295dff6cb5c28bb39c2abf89db2a956426f808
Cesar0106 <cesarea@al.insper.edu.br>
2022-09-06 14:55:24 -0300

    tudo

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='dev_aberto_cesarea',
+      version='0.1',
+      packages=['dev_aberto'],
+      install_requires=[
+        'pacote>=1.0',
+        'pacote2'
+    ],
+    scripts=['scripts/hello.py'],
+      )
